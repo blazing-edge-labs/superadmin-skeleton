b51924c6f9ff42f3c193c6dd09b17ebe3202a54a
Nikola Rajković <falconvk@gmail.com>
2017-10-06 14:22:03 +0200

    Feat/add custom login (#1)
    * Reorganize the file structure
    
    * Implement a custom login page with matching styling
    
    * Validate required login form inputs
    
    * Fix login button style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 import createHistory from 'history/createBrowserHistory'
 import { Admin, Resource, Delete } from 'admin-on-rest'
-import { UserList, UserEdit, UserCreate } from './Users'
+import { UserList, UserEdit, UserCreate } from './resources/Users'
 
-import authClient from './authClient'
-import restClient from './restClient'
+import StandardLoginPage from './components/StandardLoginPage'
+
+import authClient from './clients/authClient'
+import restClient from './clients/restClient'
 
 const history = createHistory()
 
 class App extends Component {
   render () {
     return (
-      <Admin authClient={authClient} restClient={restClient} history={history}>
+      <Admin loginPage={StandardLoginPage} authClient={authClient} restClient={restClient} history={history}>
         <Resource name="user" list={UserList} edit={UserEdit} create={UserCreate} remove={Delete}/>
       </Admin>
     )
