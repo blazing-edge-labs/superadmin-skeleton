a3fda51e9c57372a53ce0739085912b0fed0986b
Nikola Rajkovic <falconvk@gmail.com>
2017-10-06 15:47:19 +0200

    Refactor authClient

--- src/clients/authClient.js
+++ src/clients/authClient.js
@@ -1,54 +1,55 @@
 import url from 'url'
 import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'admin-on-rest'
 
+const setTokenInLocalStorage = (token) => {
+  localStorage.setItem('token', token)
+}
+
+const validateResponseStatus = (response) => {
+  if (response.status < 200 || response.status >= 300) {
+    throw new Error(response.statusText)
+  }
+}
+
+const newAuthRequest = (apiEndpoint, credentials) => (
+  new Request(url.resolve(process.env.REACT_APP_API_URL, apiEndpoint),
+  {
+    method: 'POST',
+    body: JSON.stringify(credentials),
+    headers: new Headers({ 'Content-Type': 'application/json' }),
+  })
+)
+
 export default (type, params) => {
   if (type === AUTH_LOGIN) {
     const { email, password, token } = params
 
     if (email && password) { // Standard login
-      const request = new Request(url.resolve(process.env.REACT_APP_API_URL, '/auth'), {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: new Headers({ 'Content-Type': 'application/json' }),
-      })
+      const request = newAuthRequest('/auth', { email, password })
       return fetch(request)
       .then(response => {
-        if (response.status < 200 || response.status >= 300) {
-          throw new Error(response.statusText)
-        }
+        validateResponseStatus(response)
         return response.json()
       })
       .then(({ data: { token } }) => {
-        localStorage.setItem('token', token)
+        setTokenInLocalStorage(token)
       })
     } else if (email) { // Passwordless request
-      const request = new Request(url.resolve(process.env.REACT_APP_API_URL, '/signin'), {
-        method: 'POST',
-        body: JSON.stringify({ email }),
-        headers: new Headers({ 'Content-Type': 'application/json' }),
-      })
+      const request = newAuthRequest('/signin', { email })
       return fetch(request)
       .then(response => {
-        if (response.status < 200 || response.status >= 300) {
-          throw new Error(response.statusText)
-        }
+        validateResponseStatus(response)
         return Promise.reject('Email sent!')
       })
     } else if (token) { // Passwordless confirm
-      const request = new Request(url.resolve(process.env.REACT_APP_API_URL, '/auth/token'), {
-        method: 'POST',
-        body: JSON.stringify({ token }),
-        headers: new Headers({ 'Content-Type': 'application/json' }),
-      })
+      const request = newAuthRequest('/auth/token', { token })
       return fetch(request)
       .then(response => {
-        if (response.status < 200 || response.status >= 300) {
-          throw new Error(response.statusText)
-        }
+        validateResponseStatus(response)
         return response.json()
       })
       .then(({ data: { token } }) => {
-        localStorage.setItem('token', token)
+        setTokenInLocalStorage(token)
       })
     }
   }
