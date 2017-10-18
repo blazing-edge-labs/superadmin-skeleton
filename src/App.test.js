691bcf8bc7c3c23da83b400ce85ba6fbe21e6eb5
Nikola Rajkovic <falconvk@gmail.com>
2017-10-18 15:12:43 +0200

    Add enzyme-to-json; Create first passing test; Ref T40

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+import App from './App'
+
+describe('App', () => {
+  const app = shallow(<App/>)
+
+  it('renders properly', () => {
+    expect(app).toMatchSnapshot()
+  })
+})
