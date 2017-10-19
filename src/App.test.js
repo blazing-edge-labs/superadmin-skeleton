78cbdc761b8d16e6c9dcb7e34f3cf76cd6a52591
Nikola Rajkovic <falconvk@gmail.com>
2017-10-19 14:11:33 +0200

    Add tests for the root App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { Admin } from 'admin-on-rest'
 
 import App from './App'
+import LoginPage from './containers/Login'
+import authClient from './clients/authClient'
+import restClient from './clients/restClient'
 
 describe('App', () => {
   const app = shallow(<App/>)
 
   it('renders properly', () => {
     expect(app).toMatchSnapshot()
+  })
+
+  it('has Admin as the root component', () => {
+    expect(app.type()).toBe(Admin)
+  })
+
+  describe('Admin', () => {
+    it('uses the correct `authClient`', () => {
+      expect(app.props().authClient).toBe(authClient)
+    })
+
+    it('uses the correct`restClient`', () => {
+      expect(app.props().restClient).toBe(restClient)
+    })
+
+    it('uses the custom Login page container', () => {
+      expect(app.props().loginPage).toBe(LoginPage)
+    })
+  })
+
+  describe('when rendering resources', () => {
+    it('renders the correct amount of resources', () => {
+      expect(app.find('Resource').length).toBe(1)
+    })
+
+    it('renders the `users` resource', () => {
+      const usersResource = app.findWhere(res => res.props().name === 'user')
+      expect(usersResource.length).toBe(1)
+    })
   })
 })
