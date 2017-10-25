f952dfeed0641f1bf2070e8c0e2ec36e71629e47
Nikola Rajkovic <falconvk@gmail.com>
2017-10-25 14:28:03 +0200

    Add a set of tests for AUTH_LOGIN. Ref T42

--- src/clients/authClient.test.js
+++ src/clients/authClient.test.js
@@ -1,12 +1,25 @@
 import url from 'url'
 import fetchMock from 'fetch-mock'
-import { AUTH_CHECK } from 'admin-on-rest'
+import { AUTH_LOGIN, AUTH_CHECK } from 'admin-on-rest'
 
 import authClient from './authClient'
+import { USER_ROLE_SUPERADMIN } from '../constants'
 
+// fetch-mock fix for single-parameter fetch requests
+// fetchMock.setImplementations({ Request: Request }) -- this fix works but it makes lastOptions() return undefined
+const _fetchMock = fetchMock.fetchMock
+fetchMock.fetchMock = (url, options) => {
+  if (url instanceof Request && options === undefined) {
+    return _fetchMock(url.url, url)
+  }
+  return _fetchMock(url, options)
+}
+
+// route builder
 const getRoute = (endpoint) => url.resolve(process.env.REACT_APP_API_URL, endpoint)
+
+// mock responses
 const mockResponseSuccess = {
-  status: 200,
   body: {
     data: {
       token: '000000000000000000000000',
@@ -15,7 +28,26 @@ const mockResponseSuccess = {
 }
 
 describe('authClient', async () => {
-  fetchMock.post(getRoute('/auth'), mockResponseSuccess)
+  describe('AUTH_LOGIN', async () => {
+    describe('when receiving email & password as parameters', async () => {
+      fetchMock.postOnce(getRoute('/auth'), mockResponseSuccess)
+
+      const email = 'test@example.com'
+      const password = 'testpassword'
+
+      it('sets the `token` in localStorage on successful login', async () => {
+        await authClient(AUTH_LOGIN, { email, password })
+        expect(localStorage.getItem('token')).toEqual(mockResponseSuccess.body.data.token)
+      })
+
+      it('sends a `minRole` requirement, along with credentials to the API', () => {
+        const opts = JSON.parse(fetchMock.lastOptions()._bodyInit)
+        expect(opts.minRole).toEqual(USER_ROLE_SUPERADMIN)
+        expect(opts.email).toEqual(email)
+        expect(opts.password).toEqual(password)
+      })
+    })
+  })
 
   describe('AUTH_CHECK', async () => {
     describe('when token is not present', async () => {
